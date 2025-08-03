51330455431cd6a99e72d1446be78fa9ca97002d
Mamatha <gedamamatha.231ec123@nitk.edu.in>
2025-08-03 11:18:04 +0530

    TEST BENCH.v

--- TEST BENCH.v
+++ TEST BENCH.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 18.03.2025 16:06:41
-// Design Name: 
-// Module Name: i2c_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module tb;
     
